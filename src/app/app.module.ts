9cf1c8b11ec007e945c368d634254a69ec34c60e
slidexuou <slidexuou7@gmail.com>
2021-03-20 12:19:48 +0700

    Homework3Part2

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { TreeModule } from '@circlon/angular-tree-component';
+
+import { AppComponent } from './app.component';
+import { ConfigComponent } from './config/config.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConfigComponent
+  ],
+  imports: [
+    BrowserModule,TreeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
